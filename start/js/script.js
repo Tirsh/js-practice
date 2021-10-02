4ec8e9168696e374169ff5fa939ed0a9ac937f21
Valeriy <valeriy.vasipov@gmail.com>
2021-10-02 20:42:54 +0300

    first commit

---
+++ start/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert('Hello!');
+// const msg = confirm("Are you here?");
+// console.log(msg);
+// const answer = +prompt('Are you 18?', "No");
+// console.log(typeof(answer));
+// const answers = [];
+// answers[0] = prompt("Name:", "");
+// answers[1] = prompt("Surname:", "");
+// answers[2] = +prompt("Age:", "0");
+// document.write(answers);
+// const user = 'Valeriy';
+// alert(`Привет, ${user}!!!`);
+// const arr = [1, 2];
+// console.log(arr.length);
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
