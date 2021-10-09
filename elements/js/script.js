aa0b49d8730a7d103bafb7c3194e17f6068b15aa
Valeriy <valeriy.vasipov@gmail.com>
2021-10-09 21:46:08 +0300

    New project_mDB

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,23 +1,42 @@
 'use strict';
 
-const box = document.getElementById("box");
-console.log(box);
+const box = document.getElementById("box"),
+    btns = document.getElementsByTagName("button"),
+    circles = document.getElementsByClassName("circle"),
+    hearts = document.querySelectorAll(".heart"),
+    heart = document.querySelector(".heart"),
+    wrapper = document.querySelector('.wrapper');
 
-const btns = document.getElementsByTagName("button");
-console.log(btns);
+console.dir(box);
 
-const circles = document.getElementsByClassName("circle");
-console.log(circles);
+box.style.backgroundColor = 'yellow';
+box.style.width = "50px";
 
-const hearts = document.querySelectorAll(".heart");
-hearts.forEach( item => {
-    console.log(item);
-});
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
-function removeAllBefore(values, b) {
-    const str = values.join('');
-    const nw = str.slice(str.indexOf(''+b));
-    return nw.split('').map( index => +index);
-}
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'green';
+// }
 
-console.log(removeAllBefore([1, 2, 3, 4, 5], 3));
+hearts.forEach(item => item.style.backgroundColor = 'green');
+
+const div = document.createElement('div');
+const text = document.createTextNode('I was here!');
+
+div.classList.add('black');
+document.body.append(div);
+// document.body.appendChild(div);
+document.querySelector('.wrapper').append(div);
+wrapper.prepend(div);
+hearts[0].before(div);
+hearts[1].after(div);
+// wrapper.insertBefore(div, hearts[0]);
+circles[1].remove();
+// wrapper.removeChild(hearts[1]);
+circles[1].replaceWith(hearts[0]);
+// wrapper.replaceChild(circles[1], hearts[0]);
+
+div.innerHTML = '<h1>Hello World!!!</h1>';
+// div.textContent = "Hello";
+div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>');
