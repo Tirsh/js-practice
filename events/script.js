dd7132a55592d6e84434e5a760554c69233ed0a5
Valeriy <valeriy.vasipov@gmail.com>
2021-10-10 20:18:04 +0300

    030 Events

---
+++ events/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button');
+// console.log(btn);
+// btn.onclick = function() {
+//     alert('Hello World!!!');
+
+// };()
+btn.addEventListener('click', function() {alert('Hello World!!!');});
