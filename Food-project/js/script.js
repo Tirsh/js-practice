e248bf1995a6b4b7f0cbe64e19936c9da45ad714
Valeriy <valeriy.vasipov@gmail.com>
2021-10-17 09:47:19 +0300

    Tabs food project finish

--- Food-project/js/script.js
+++ Food-project/js/script.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__item');
+          tabsParent = document.querySelector('.tabheader__items');
     
     function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
@@ -13,24 +14,24 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function showTabContent(tab = 0) {
-        tabsContent[tab].style.display = 'block';
+        tabsContent[tab].classList.add('show', 'fade');
+        tabsContent[tab].classList.remove('hide');
         tabs[tab].classList.add('tabheader__item_active');
     }
-
     hideTabContent();
     showTabContent();
-
     tabsParent.addEventListener('click', event => {
+        event.preventDefault();
         const target = event.target;
         console.log(target);
-        // if (target && target.classList.contains('tabheader__item')) {
-        //     tabs.forEach((item, i) => {
-        //         if(item == target){
-        //             hideTabContent();
-        //             showTabContent(i);
-        //         }
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(item == target){
+                    hideTabContent();
+                    showTabContent(i);
+                }
                 
-        //     });
-        // }
+            });
+        }
     });
 });
