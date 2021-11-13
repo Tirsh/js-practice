cac771610f204c4a7106f48ab83de6317328ff90
Valeriy <valeriy.vasipov@gmail.com>
2021-11-13 18:28:28 +0300

    regExp 2

--- RegExp/script.js
+++ RegExp/script.js
@@ -3,15 +3,21 @@
 // new RegExp('patern', 'flags');
 // /patern/f
 
-const ans = prompt('Input name');
+// const ans = prompt('Input name');
 
-const reg = /n/ig;
+// const reg = /n/ig;
 // i - registr
 // g - group
 // m - multistr
 
 //console.log(ans.search(reg));
-console.log(ans.match(reg));
+// console.log(ans.match(reg));
 
-const pass = prompt('Insert password');
-console.log(pass.replace(/./g, '*'));
+// const pass = prompt('Insert password');
+// console.log(pass.replace(/./g, '*'));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
+//\d
+//\w
+//\s
