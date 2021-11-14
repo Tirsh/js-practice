ad53557abbb89c742bdd984110316b30e0276d48
Valeriy <valeriy.vasipov@gmail.com>
2021-11-14 20:13:38 +0300

    Export import

---
+++ Export - import/js/main.js
@@ -0,0 +1,8 @@
+export let one = 5;
+const two = 20;
+
+export {two};
+
+export default function sayHi(){
+    console.log('Hello!!!');
+}
---
+++ Export - import/js/script.js
@@ -0,0 +1,5 @@
+import * as data from './main';
+import sayHi from './main';
+
+console.log(`${data.one} ${data.two}`);
+sayHi();
